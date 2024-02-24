14b592f9e49ccef38f12dd7f6b336f9d36298b37
Timofii0507 <timofiiyurkov@gamil.com>
2024-02-24 13:29:50 +0200

    Перше  завдання  з практичної  роботи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿
+using System.Text;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("It's easy to win forgiveness for being wrong;");
+            Console.WriteLine("being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
+        }
+    }
+}
