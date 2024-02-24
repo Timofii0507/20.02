aabbb2f6b839f2556f3e7b5af3e09878d51ea51d
Timofii0507 <timofiiyurkov@gamil.com>
2024-02-24 21:43:07 +0200

    Перше  завдання  з  дз

---
+++ Program7.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace _20._02
+{
+    class Program7
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Введіть число від 1 до 100:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Помилка: число повинно бути в діапазоні від 1 до 100!");
+                return;
+            }
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
