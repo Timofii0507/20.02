b5d207e7121af8d2705decf5595d933a2dc9ae01
Timofii0507 <timofiiyurkov@gamil.com>
2024-02-24 18:42:01 +0200

    Друге  завдання з  практичної  роботи

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace _20._02
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("It's easy to win forgiveness for being wrong;");
             Console.WriteLine("being right is what gets you into real trouble.");
