346a12a5a2c60eada4763fcf2e95aa9176911362
Timofii0507 <timofiiyurkov@gamil.com>
2024-02-24 23:21:06 +0200

    Шосте завдання з дз

---
+++ Program12.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace _20._02
+{
+    class Program12
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Введіть показання температури:");
+            double temperature = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть 1, якщо хочете конвертувати з Фаренгейта в Цельсій, або 2, якщо навпаки:");
+            int choice = int.Parse(Console.ReadLine());
+
+            double result;
+            if (choice == 1)
+            {
+                result = (temperature - 32) * 5 / 9;
+                Console.WriteLine("{0} градусів Фаренгейта дорівнює {1} градусів Цельсія", temperature, result);
+            }
+            else if (choice == 2)
+            {
+                result = temperature * 9 / 5 + 32;
+                Console.WriteLine("{0} градусів Цельсія дорівнює {1} градусів Фаренгейта", temperature, result);
+            }
+            else
+            {
+                Console.WriteLine("Помилка: ви повинні ввести 1 або 2!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
